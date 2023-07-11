60d8ef7baa6199f5a8170900a3ecd5753d9bb8f4
atmosg <atmosg@naver.com>
2023-07-11 13:40:38 +0900

    검증 Ver1

--- src/main/java/hello/itemService/domain/item/basic/BasicItemController.java
+++ src/main/java/hello/itemService/domain/item/basic/BasicItemController.java
@@ -1,6 +1,7 @@
 package hello.itemService.domain.item.basic;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.util.StringUtils;
 
 import hello.itemService.domain.item.DeliveryCode;
 import hello.itemService.domain.item.Item;
@@ -88,8 +90,35 @@ public class BasicItemController {
   }
 
   @PostMapping("/add")
-  public String addItem(@ModelAttribute("item") Item item, RedirectAttributes redirectAttributes) {
+  public String addItem(@ModelAttribute("item") Item item, RedirectAttributes redirectAttributes, Model model) {
     log.info("item : {}", item.toString());
+
+    Map<String, String> errors = new HashMap<>();
+
+    if (!StringUtils.hasText(item.getItemName())) {
+      errors.put("itemName", "상품명을 입력해 주세요.");
+    }
+
+    Integer price = item.getPrice();
+    Integer quantity = item.getQuantity();
+    if (price == null || price < 1_000 || price > 1_000_000) {
+      errors.put("price", "가격은 1,000 ~ 1,000,000원 까지 허용됩니다.");
+    }
+
+    if (quantity == null || quantity >= 9_999) {
+      errors.put("quantity", "수량은 9,999개 까지 허용됩니다.");
+    }
+
+    if (price !=null && quantity !=null && price * quantity < 10_000) {
+      errors.put("globalError", "주문 금액은 최소 10,000원 이상이어야 합니다.\n 현재 주문하신 금액은 " + price * quantity + "원 입니다.");
+    }
+
+    log.info("errors {}", errors);
+
+    if (!errors.isEmpty()) {
+      model.addAttribute("errors", errors);
+      return "basic/addForm";
+    }
 
     itemRepository.save(item);
     redirectAttributes.addAttribute("itemId", item.getId());
